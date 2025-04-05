05c83229bc693dc223cd0d5188e6feb015577efa
Tomasz <tomasz1999u@gmail.com>
2025-04-05 12:06:48 +0200

    Add first form

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+
+st.title("Szacowanie ceny mieszkania")
+with st.form(key="parametry_mieszkania"):
+    st.subheader("Wybierz parametry mieszkania")
+
+    st.date_input("Wybierz rok budowy")
+    st.select_slider("Liczba pokoi", options=[1,2,3,4,5,6,7,8,9,10])
+    st.number_input("Piętro", min_value=0, max_value=50)
+    st.number_input("Metraż (w metrach kwadratowych)",min_value=20, max_value=500)
+    st.selectbox("Dzielnica", [ "Bemowo", "Białołęka", "Bielany", "Mokotów", "Ochota", "Praga-Południe", "Praga-Północ", "Rembertów", "Śródmieście", "Targówek", "Ursus", "Ursynów", "Wawer", "Wesoła", "Wilanów", "Włochy", "Wola", "Żoliborz"])
+    st.selectbox("Wyposażenie", ['Tak', 'Nie'])
+    pressed_button = st.form_submit_button(label="Wyślij")
+    if pressed_button:
+        st.warning("Wkrótce dostępne")
